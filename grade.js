af0eba1732fbb23b0886e3bc81ce73db52fbb5f8
CI Grader <zhezhou@umich.edu>
2020-09-11 02:47:14 -0400

    Update grades at time: 11-09-2020-02-46-43

---
+++ grade.js
@@ -0,0 +1,219 @@
+grade = 
+{
+  "chrisree": {
+    "name": "Lee, Christopher", 
+    "unique_name": "chrisree", 
+    "email": "chrisree@umich.edu", 
+    "course": "EECS 367 F20", 
+    "repository": "git@github.com:chrislee115/autorob.git", 
+    "assignments": {
+      "Honor": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "PathPlan_Heap": {
+        "status": "PASS", 
+        "test results": {
+          "Test 8": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 9": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 2": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 3": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 0": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 1": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 6": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 7": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 4": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }, 
+          "Test 5": {
+            "Status": "Succeed", 
+            "Stderr": "N/A"
+          }
+        }, 
+        "comments": [
+          "10 out of 10 test cases passed"
+        ]
+      }, 
+      "PathPlan_AStar": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }
+        }, 
+        "comments": [
+          "0 out of 5 test cases passed"
+        ]
+      }, 
+      "Pendularm_Euler": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_PID": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_MatrixRoutines": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_Transforms": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_JointRendering": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_BaseOffset": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_Quaternion": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_RexArm": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_SetpointControl": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_FSM": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_Jacobian": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JTranspose": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JPseudoInverse": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_Collision": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pitch": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }
+    }, 
+    "quizzes": [
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
