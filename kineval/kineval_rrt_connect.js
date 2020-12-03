7e0c77540fea97ed266b9fdf44a1fd91ecd281d8
Chris Lee <chrisree@umich.edu>
2020-12-02 22:53:42 -0500

    starting rrt connect blah

--- kineval/kineval_rrt_connect.js
+++ kineval/kineval_rrt_connect.js
@@ -91,6 +91,8 @@ kineval.planMotionRRTConnect = function motionPlanningRRTConnect() {
     }
 }
 
+var Tree_a;
+var Tree_b;
 
     // STENCIL: uncomment and complete initialization function
 kineval.robotRRTPlannerInit = function robot_rrt_planner_init() {
@@ -125,9 +127,10 @@ kineval.robotRRTPlannerInit = function robot_rrt_planner_init() {
 
     // make sure the rrt iterations are not running faster than animation update
     cur_time = Date.now();
+
+    Tree_a = tree_init(q_start_config);
+    Tree_b = tree_init(q_goal_config);
 }
-
-
 
 function robot_rrt_planner_iterate() {
 
@@ -150,8 +153,7 @@ function robot_rrt_planner_iterate() {
     //   tree_init - creates a tree of configurations
     //   tree_add_vertex - adds and displays new configuration vertex for a tree
     //   tree_add_edge - adds and displays new tree edge between configurations
-    }
-
+    iterate_rrt_connect(Tree_a, Tree_b);
 }
 
 //////////////////////////////////////////////////
@@ -237,6 +239,199 @@ function tree_add_edge(tree,q1_idx,q2_idx) {
     //   find_path
     //   path_dfs
 
+function getDistToQRand(q1, q2) {
+    return Math.pow(q1[0] - q2[0], 2) + 
+                            Math.pow(q1[1] - q2[1], 2);
+}
+function vector_normalize(v) {
+    // returns normalized vector for v
+    var magnitude = 0
+    for (var i = 0; i < v.length; ++i) {
+        magnitude += (Math.pow(v[i], 2));
+    }
+    magnitude = Math.sqrt(magnitude);
+    for (var i = 0; i < v.length; ++i) {
+        v[i] = v[i] / magnitude;
+    }
+    return v;
+}
+// double check legality lol
+function getQNew(q, q_near) {
+    var q_new = [ (q[0] - q_near[0]), (q[1] - q_near[1]) ];
+    q_new = vector_normalize(q_new);
+    q_new = [ q_near[0] + (q_new[0] * eps), 
+              q_near[1] + (q_new[1] * eps) ]
+    return q_new;
+}
+
+function isEqual(q1, q2) {
+    return (Math.abs(q1[0] - q2[0]) < eps) && 
+            (Math.abs(q1[1] - q2[1]) < eps);
+}
+// q is q_rand
+function rrt_extend(T, q) {
+    var q_near_tmp = nearest_neighbor(T, q);
+    var q_near = q_near_tmp[0];
+    var q_near_idx = q_near_tmp[1];
+    var q_new = getQNew(q, q_near);
+    
+    // I don't see why a new config function is needed
+    if (!kineval.poseIsCollision(q_new)) {
+        tree_add_vertex(T, q_new);
+        var q_new_idx = T.newest;
+        tree_add_edge(T, q_near_idx, q_new_idx);
+
+        // Double check this equality
+        if (isEqual(q_new, q)) {
+            return "Reached";
+        } else {
+            return "Advanced";
+        }
+    }
+    return "Trapped";
+}
+
+function rrt_connect(T, q) {
+    do {
+        S = rrt_extend(T, q);
+    } while (S == "Advanced");
+    return S;
+}
+
+function random_config() {
+    // // collision only checked in x-z plane
+    // robot_boundary = [[-10,0,-10],[2,0,2]];
+
+    var x = (Math.random() * 13) - 10;
+    var z = (Math.random() * 13) - 10;
+    return [x,z];
+}
+
+
+function nearest_neighbor(T, q) {
+    var best = 10000000000000;
+    var best_index = -1;
+    for (var i = 0; i < T.vertices.length; ++i) {
+        // This is how we do dist to goal right
+        var cur_dist = getDistToQRand(T.vertices[i].vertex, q);
+        if (cur_dist < best) {
+            best = cur_dist;
+            best_index = i;
+        }
+    }
+    return [T.vertices[best_index].vertex, best_index];
+}
+
+function findVertexIdx(T, vtx) {
+    for (var i = 0; i < T.vertices.length; ++i) {
+        if (T.vertices[i].vertex == vtx) {
+            return i;
+        }    
+    }
+    return -1;
+}
+function ihatejs(arr, vtx) {
+    for (var i = 0; i < arr.length; ++i) {
+        if (arr[i].vertex == vtx) {
+            return i;
+        }
+    }
+    return -1;
+}
+function path_dfs(T, q) {
+    //TODO: for reconstructing path when were done
+    var isInit = (T.vertices[0].vertex == q_init);
+    var goal = isInit ? q : q_goal;
+    var dfs = [];
+    var visited = [];
+    var path = [];
+    var start = isInit ? q_init : q;
+    dfs.push(start);
+    while (dfs.length != 0) {
+        var cur_vtx = dfs.pop();
+        var cur_vtx_idx = findVertexIdx(T, cur_vtx);
+
+        var tmp_obj = {}
+        tmp_obj.vertex = cur_vtx;
+
+        visited.push(tmp_obj);
+
+        var parent_idx = ihatejs(visited, cur_vtx);
+        if (isEqual(cur_vtx, goal)) {
+            var cur_node = visited[parent_idx];
+            while (true) {
+                var tmp_obj2 = {};
+                tmp_obj2.vertex = cur_node.vertex;
+                if (path.indexOf(tmp_obj2) == -1) {
+                    path.unshift(tmp_obj2);
+                }
+                if (cur_node.parent == undefined) break;
+                cur_node = visited[cur_node.parent];
+            }
+            return path;
+        }
+
+        var neighbors = T.vertices[cur_vtx_idx].edges;
+        for (var i = 0; i < neighbors.length; ++i) {
+            var neighbor = neighbors[i].vertex;
+            if (ihatejs(visited, neighbor) == -1) {
+                dfs.push(neighbor);
+
+                var tmp_obj2 = {}
+                tmp_obj2.vertex = neighbor;
+                visited.push(tmp_obj2);
+                visited[visited.length-1].parent = parent_idx;
+            }
+        }
+    }
+    // should not get here
+    return dfs;
+}
+function iterate_rrt_connect(T_a, T_b) {
+
+
+    // STENCIL: implement a single iteration of an RRT-Connect algorithm.
+    //   An asynch timing mechanism is used instead of a for loop to avoid
+    //   blocking and non-responsiveness in the browser.
+    //
+    //   Return "failed" if the search fails on this iteration.
+    //   Return "succeeded" if the search succeeds on this iteration.
+    //   Return "extended" otherwise.
+    //
+    //   Provided support functions:
+    //
+    //   testCollision - returns whether a given configuration is in collision
+    //   tree_add_vertex - adds and displays new configuration vertex for a tree
+    //   tree_add_edge - adds and displays new tree edge between configurations
+    //   drawHighlightedPath - renders a highlighted path in a tree
+
+    // // search_max_iterations == K?
+    if (search_iter_count > search_max_iterations) {
+        search_iterate = false;
+        return "failed";
+    }
+    var q_rand = random_config();
+    if (rrt_extend(T_a, q_rand) != "Trapped") {
+        var q_newA = T_a.vertices[T_a.newest].vertex;
+        if (rrt_connect(T_b, q_newA) == "Reached") {
+            var q_newB = T_b.vertices[T_b.newest].vertex;
+            // swap it, bc ordering gets messed up
+            if (T_a.vertices[0].vertex != q_init) {
+                [T_a, T_b] = [T_b, T_a];
+                [q_newA, q_newB] = [q_newB, q_newA];
+            }
+            var pathA = path_dfs(T_a, q_newA);
+            var pathB = path_dfs(T_b, q_newB);
+            var path = pathA.concat(pathB);
+            drawHighlightedPath(path);
+            search_iterate = false;
+            return "succeeded";
+        } 
+    }
+    // swap
+    [T_a, T_b] = [T_b, T_a];
+    return "extended";
+}
 
 
 
